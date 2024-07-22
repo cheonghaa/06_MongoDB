b0be51a95151a0594ea194444b4bebf7ef41b5f8
DYA <148474216+cheonghaa@users.noreply.github.com>
2024-07-22 14:29:11 +0900

    24.07.22 Ch.02 L1 실습

--- ch02.js
+++ ch02.js
@@ -1,3 +1,6 @@
+//ch02 실습
+//L1
+
 use my_database;
 db.users.find();
 
@@ -5,5 +8,30 @@ db.users.insert({username: "smith"});
 
 use tutorial
 db.users.insert({username: 'smith'});
+db.users.insert({username: 'jones'});
 
 db.users.find();
+db.users.find({username: "jones"})
+db.users.find({ $or: [{username:"smith"}, {username: "jones"}]})
+
+db.users.update({username: "smith"}, {$set: {country:"Canada"}})
+db.users.find({username: "smith"})
+
+db.users.find({username: "smith"}, {country : "Canada"})
+db.users.find({country: "Canada"}, {username: "smith", country: "Canada"})
+
+db.users.update({username: "smith"}, {$unset: {country:1}})
+db.users.find({username: "smith"})
+
+show dbs;
+show collections;
+
+//현재 사용중인 데이터베이스
+db.stats();
+
+//컬렉션 상태
+db.numbers.stats();
+
+
+
+//L2
